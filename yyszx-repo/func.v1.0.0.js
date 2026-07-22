1c4ab9513da8f4ad0aaa0323d1ce97cde4d3f227
zwy2673976185-design <zwy2673976185@gmail.com>
2026-07-23 04:23:37 +0800

    Delete yyszx-repo/func.v1.0.0.js

--- yyszx-repo/func.v1.0.0.js
+++
@@ -1,4 +0,0 @@
-window.initMainUI = function() {
-  const mainUI = document.getElementById('mainUI');
-  mainUI.innerHTML = '<div class="window-header">yyszx</div><div style="padding:20px;text-align:center;">功能界面已加载</div>';
-};
